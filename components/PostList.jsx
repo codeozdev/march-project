0a177d5d7838c7ec29800346ebe2a52db56d5c60
codeoz <ozudev@gmail.com>
2024-03-22 03:47:50 +0300

    vercel 6

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -4,32 +4,32 @@ import PostForm from '@/components/PostForm'
 
 const URL = process.env.NEXT_PUBLIC_URL
 
-async function getAllPosts() {
-  const res = await fetch(`${URL}/api/posts/`, {
-    cache: 'no-cache',
-  })
-  try {
-    return await res.json()
-  } catch (error) {
-    console.log(error)
-  }
-}
+// async function getAllPosts() {
+//   const res = await fetch(`${URL}/api/posts/`, {
+//     cache: 'no-cache',
+//   })
+//   try {
+//     return await res.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
 export default async function PostList() {
-  const data = await getAllPosts()
+  // const data = await getAllPosts()
 
   return (
     <div className='flex items-center justify-center flex-col h-[calc(100vh-94px)]'>
       <Image src='/1.png' width={100} height={100} alt='logo' />
-      <PostForm data={data} />
-      {/* POSTS */}
-      <div className='mt-10 flex gap-5 flex-col lg:w-1/3 lg:mx-auto w-full'>
-        {data.map((post) => (
-          <div className='bg-slate-800 lg:rounded-t-lg' key={post.id}>
-            <PostContent post={post.post} created={post.createdAt} id={post.id} />
-          </div>
-        ))}
-      </div>
+      {/*<PostForm data={data} />*/}
+      {/*/!* POSTS *!/*/}
+      {/*<div className='mt-10 flex gap-5 flex-col lg:w-1/3 lg:mx-auto w-full'>*/}
+      {/*  {data.map((post) => (*/}
+      {/*    <div className='bg-slate-800 lg:rounded-t-lg' key={post.id}>*/}
+      {/*      <PostContent post={post.post} created={post.createdAt} id={post.id} />*/}
+      {/*    </div>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
     </div>
   )
 }
