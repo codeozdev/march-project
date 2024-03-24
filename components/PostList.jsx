62a3b9aa1c039548a9e578693c7960f992cb7d8e
codeoz <ozudev@gmail.com>
2024-03-24 23:36:24 +0300

    Problem vercel 3

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -17,14 +17,15 @@ export default async function PostList() {
   const { posts, message, error } = await getPosts()
   const data = posts
   console.log(message || error)
+  console.log(data)
 
   return (
     <div className='flex items-center justify-center flex-col h-[calc(100vh-94px)]'>
       <Image src='/1.png' width={100} height={100} alt='logo' />
-      <PostForm data={data} />
+      <PostForm data={posts} />
       {/*POSTS*/}
       <div className='mt-10 flex gap-5 flex-col lg:w-1/3 lg:mx-auto w-full'>
-        {data.map((post) => (
+        {posts.map((post) => (
           <div className='bg-slate-800 lg:rounded-t-lg' key={post.id}>
             <PostContent post={post.post} created={post.createdAt} id={post.id} />
           </div>
